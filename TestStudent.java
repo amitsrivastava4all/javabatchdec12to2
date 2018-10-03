02cdacc857647c9fc22654efa21af79a30703a7c
Himanshu Chandra <himanshu.chandra@outlook.com>
2018-10-03 17:24:05 +0530

    Update TestStudent.java

--- TestStudent.java
+++ TestStudent.java
@@ -7,7 +7,7 @@ public class TestStudent {
 		ram.id = -1001;  //77.id = 1001
 		ram.name = "Ram";
 		ram.course = "Java";
-		ram.print();
+		ram.print("Just looking");
 		//Student shyam = new Student();
 		//System.out.println("Id is "+shyam.id);  // 99.id
 		int x = 100;
